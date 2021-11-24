7b25429a6c3a9bd67a4d267111405ce45bf1e2b0
Justine <hell.justine@gmail.com>
2021-11-24 17:38:31 +0100

    Validating af form level Validate Password Field

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -2,14 +2,19 @@
   <form @submit.prevent="onSubmit">
     <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput label="Password" type="password" />
+    <BaseInput
+      label="Password"
+      type="password"
+      v-model="password"
+      :error="passwordError"
+    />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
-import { useField } from 'vee-validate';
+import { useField, useForm } from 'vee-validate';
 
 export default {
   setup() {
@@ -17,11 +22,9 @@ export default {
       alert('Submitted');
     }
 
-    // Could also be written without descructuring like so -->
-    // const email = useField('email', (value) => {...})
-    const { value: email, errorMessage: emailError } = useField(
-      'email',
-      (value) => {
+    const validations = {
+      // WARNING : this naming 'email' & 'password' must match the name of the 'model' passed as first argument inside 'useField' methods l.50 & 52
+      email: (value) => {
         if (!value) return 'This field is required';
 
         const regex =
@@ -30,15 +33,30 @@ export default {
           return 'Please enter a valid email address';
         }
         return true;
-      }
-    );
+      },
+      password: (value) => {
+        const requiredMessage = 'This field is required';
+        if (value === undefined || value === null) return requiredMessage;
+        if (!String(value).length) return requiredMessage;
+
+        return true;
+      },
+    };
+
+    useForm({
+      validationSchema: validations,
+    });
+
+    const { value: email, errorMessage: emailError } = useField('email');
+    const { value: password, errorMessage: passwordError } =
+      useField('password');
 
     return {
       onSubmit,
       email,
-      // email: email.value,
       emailError,
-      // emailerror: email.errorMessage,
+      password,
+      passwordError,
     };
   },
 };
