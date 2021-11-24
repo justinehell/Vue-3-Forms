91b91df6063db64c248ec36046d3296a13c5dde9
Justine <hell.justine@gmail.com>
2021-11-24 14:50:23 +0100

    Base Radio

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -20,13 +20,11 @@
 
       <h3>Are pets allowed?</h3>
       <div>
-        <input type="radio" v-model="event.pets" :value="1" name="pets" />
-        <label>Yes</label>
+        <BaseRadio v-model="event.pets" :value="1" label="Yes" name="pets" />
       </div>
 
       <div>
-        <input type="radio" v-model="event.pets" :value="0" name="pets" />
-        <label>No</label>
+        <BaseRadio v-model="event.pets" :value="0" label="No" name="pets" />
       </div>
 
       <h3>Extras</h3>
